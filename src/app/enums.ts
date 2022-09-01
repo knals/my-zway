7dde636d001ac081e3743621130beddc0ba6b5e5
knals <knals.developer@gmail.com>
2022-09-01 21:41:57 +0200

    added new devices

--- src/app/enums.ts
+++ src/app/enums.ts
@@ -7,6 +7,7 @@ export enum Command {
 export enum Devices {
   MAIN_ROOM_DOOR = 'ZWayVDev_zway_8-0-38',
   MAIN_ROOM_WINDOW = 'ZWayVDev_zway_3-0-38',
-
-
+  KITCHEN_DOOR = 'ZWayVDev_zway_2-0-38',
+  KITCHEN_WINDOW = 'ZWayVDev_zway_4-0-38',
+  SMALL_ROOM = 'ZWayVDev_zway_7-0-38'
 }

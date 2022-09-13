3a44c878095778c1fbae45f0d264ec1f652ee4f4
knals <knals.developer@gmail.com>
2022-09-13 17:38:34 +0200

    .

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,31 +12,31 @@ export class AppService {
 
 
   public executeCommand(device: Devices, command: Command): Observable<any> {
-    const url = `http://raspberrypi.local/ZAutomation/api/v1/devices/${device}/command/${command}`;
+    const url = `http://192.168.1.109:8083/ZAutomation/api/v1/devices/${device}/command/${command}`;
     return this.HttpClient.get<any>(url, {});
 
   }
 
   public executeCommandStr(device: string, command: string): Observable<any> {
-    const url = `http://raspberrypi.local/ZAutomation/api/v1/devices/${device}/command/${command}`;
+    const url = `http://192.168.1.109:8083/ZAutomation/api/v1/devices/${device}/command/${command}`;
     return this.HttpClient.get<any>(url, {});
 
   }
 
   public commandOn(device: string | 'ZWayVDev_zway_8-0-38'): Observable<any> {
-    const url = `http://raspberrypi.local/ZAutomation/api/v1/devices/${device}/command/on`;
+    const url = `http://192.168.1.109:8083/ZAutomation/api/v1/devices/${device}/command/on`;
     return this.HttpClient.get<any>(url, {});
 
   }
 
   public commandOff(device: string | 'ZWayVDev_zway_8-0-38'): Observable<any> {
-    const url = `http://raspberrypi.local/ZAutomation/api/v1/devices/ZWayVDev_zway_8-0-38/command/off`;
+    const url = `http://192.168.1.109:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_8-0-38/command/off`;
     return this.HttpClient.get<any>(url, {});
 
   }
 
   public commandStop(device: string | 'ZWayVDev_zway_8-0-38'): Observable<any> {
-    const url = `http://raspberrypi.local/ZAutomation/api/v1/devices/ZWayVDev_zway_8-0-38/command/stop`;
+    const url = `http://192.168.1.109:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_8-0-38/command/stop`;
     return this.HttpClient.get<any>(url, {});
 
   }

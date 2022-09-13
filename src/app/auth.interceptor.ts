efc014b759e0a125540b75e3d1e2d28767514fe4
knals <knals.developer@gmail.com>
2022-09-13 16:33:50 +0200

    some changes

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -7,17 +7,31 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-      const request = req.clone({
-            headers: new HttpHeaders({
-              'Accept':  'application/json, text/plain, */*',
-              'Cookie': 'ZWAYSession=8e61551d-fa06-d9d5-0f21-6a2deaac5b8d',
-              'ZWAYSession' : '8e61551d-fa06-d9d5-0f21-6a2deaac5b8d',
-              'Feature-Policy': 'camera "none"; geolocation "none"; microphone "self"; usb "none"',
-              'Access-Control-Allow-Origin' : '*',
-              'Access-Control-Allow-Methods' : 'GET,HEAD,OPTIONS,POST,PUT',
-              'Access-Control-Allow-Headers' : 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Feature-Policy, ZWAYSession, Cookie',
-            })
-          });
+    let request:HttpRequest<any>;
+
+    if (req.url.indexOf('ZAutomation') !== -1 ) {
+       request = req.clone({
+        /* 'Cookie': 'ZWAYSession=8e61551d-fa06-d9d5-0f21-6a2deaac5b8d', */
+        headers: new HttpHeaders({
+          'Accept':  'application/json, text/plain, */*',
+          'ZWAYSession' : '8e61551d-fa06-d9d5-0f21-6a2deaac5b8d',
+        })
+      });
+    } else {
+      console.log('REQUEST AQUIUIIII');
+      request = req.clone({
+        headers: new HttpHeaders({
+          'Accept':  'application/json, text/plain, */*',
+          'Cookie': 'ZWAYSession=8e61551d-fa06-d9d5-0f21-6a2deaac5b8d',
+          'ZWAYSession' : '8e61551d-fa06-d9d5-0f21-6a2deaac5b8d',
+          'Feature-Policy': 'camera "none"; geolocation "none"; microphone "self"; usb "none"',
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods' : 'GET,HEAD,OPTIONS,POST,PUT',
+          'Access-Control-Allow-Headers' : 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Feature-Policy, ZWAYSession, Cookie',
+        })
+      });
+    }
+
       return next.handle(request);
         }
 
--- src/app/enums.ts
+++ src/app/enums.ts
@@ -1,13 +1,15 @@
 export enum Command {
   ON = 'on',
   OFF = 'off',
-  STOP = 'stop',
+  STOP = 'stop'
+
 }
 
 export enum Devices {
-  MAIN_ROOM_DOOR = '',
-  MAIN_ROOM_WINDOW = '',
-  KITCHEN_DOOR = 'ZWayVDev_zway_2-0-38',
-  KITCHEN_WINDOW = 'ZWayVDev_zway_4-0-38',
-  SMALL_ROOM = ''
+  MAIN_ROOM_DOOR    = 'ZWayVDev_zway_8-0-38',
+  MAIN_ROOM_WINDOW  = 'ZWayVDev_zway_3-0-38',
+  KITCHEN_DOOR      = 'ZWayVDev_zway_2-0-38',
+  KITCHEN_WINDOW    = 'ZWayVDev_zway_4-0-38',
+  SMALL_ROOM        = 'ZWayVDev_zway_7-0-38'
+
 }
